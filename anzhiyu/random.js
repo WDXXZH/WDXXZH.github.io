2d0ec0190d0cca9f7f925e2a9b651191af81e809
WDXXZH <1433319082@qq.com>
2024-07-24 15:06:29 +0800

    Site updated: 2024-07-24 15:06:28

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/c3f78667/","posts/68728ca3/","posts/0/","posts/83e63798/","posts/7b740b99/","posts/dd0654c3/","posts/4a17b156/","posts/1fd5e519/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
