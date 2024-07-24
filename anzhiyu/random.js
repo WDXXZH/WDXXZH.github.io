21987741c87bd4e056877f68233e531d0ed82709
WDXXZH <1433319082@qq.com>
2024-07-24 14:01:25 +0800

    Site updated: 2024-07-24 14:01:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/0/","posts/1fd5e519/","posts/c3f78667/","posts/83e63798/","posts/7b740b99/","posts/68728ca3/","posts/4a17b156/","posts/dd0654c3/"];function toRandomPost(){
+var posts=["posts/c3f78667/","posts/68728ca3/","posts/0/","posts/83e63798/","posts/7b740b99/","posts/dd0654c3/","posts/4a17b156/","posts/1fd5e519/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
